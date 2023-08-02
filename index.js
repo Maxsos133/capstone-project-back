e2f5fd34adc787bbc127a605e1286be06a603a0e
Maxsos133 <max.pavlenko133@gmail.com>
2023-08-01 19:59:27 -0500

    test payment

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.post('/webhook', express.raw({type: 'application/json'}), (request, response
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(request.body, sig, stripeWebhookSecret);
   } catch (err) {
     response.status(400).send(`Webhook Error: ${err.message}`);
     return;
