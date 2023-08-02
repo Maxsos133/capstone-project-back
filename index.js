7bad652a7a8e643ded7eb2445fbb571f3156d54c
Maxsos133 <max.pavlenko133@gmail.com>
2023-08-01 21:16:11 -0500

    test payment

--- index.js
+++ index.js
@@ -86,9 +86,7 @@ app.post('/webhook', (req, res) => {
   response.send();
 });
 
-app.use(express.json());
 app.use(logger('dev'));
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use((req, res, next) => {
   res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
